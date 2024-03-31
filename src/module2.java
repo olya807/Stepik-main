f318a3062c9953de8142866697d14f5e7b878596
Volha <morozolga92@gmail.com>
2024-03-31 13:23:42 +0300

    Task 3.3

---
+++ src/module2.java
@@ -0,0 +1,59 @@
+import java.math.*;
+import java.util.Arrays;
+
+public class module2 {
+    public static void main(String[] args) {
+        Robot robot = new Robot(0,0, Direction.DOWN);
+        moveRobot(robot, -10, 20);
+    }
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public static class Robot {
+        int x;
+        int y;
+        Direction dir;
+
+        public Robot (int x, int y, Direction dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        public Direction getDirection() {return dir;}
+
+        public int getX() {return x;}
+
+        public int getY() {return y;}
+
+        public void turnLeft() {
+            if      (dir == Direction.UP)    {dir = Direction.LEFT;}
+            else if (dir == Direction.DOWN)  {dir = Direction.RIGHT;}
+            else if (dir == Direction.LEFT)  {dir = Direction.DOWN;}
+            else if (dir == Direction.RIGHT) {dir = Direction.UP;}
+        }
+
+        public void turnRight() {
+            if      (dir == Direction.UP)    {dir = Direction.RIGHT;}
+            else if (dir == Direction.DOWN)  {dir = Direction.LEFT;}
+            else if (dir == Direction.LEFT)  {dir = Direction.UP;}
+            else if (dir == Direction.RIGHT) {dir = Direction.DOWN;}
+        }
+
+        public void stepForward() {
+            if (dir == Direction.UP)    {y++;}
+            if (dir == Direction.DOWN)  {y--;}
+            if (dir == Direction.LEFT)  {x--;}
+            if (dir == Direction.RIGHT) {x++;}
+        }
+    }
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        // your code
+    }
+}
