72ce9073f50eae78a2d45640bf5267954eaf8616
Volha <morozolga92@gmail.com>
2024-03-31 13:29:33 +0300

    Task 3.5 - 1

---
+++ src/CalcIntegral.java
@@ -0,0 +1,16 @@
+import java.util.function.DoubleUnaryOperator;
+
+public class CalcIntegral {
+
+//метод левых прямоугольников
+
+    public static void main(String[] args) {
+        System.out.println(integrate(x -> 1, 0, 10));//10.0
+        System.out.println(integrate(x -> x + 2, 0, 10));//70.0
+        System.out.println(integrate( x -> Math.sin(x) / x , 1, 5));//0.603848
+    }
+    public static double integrate(DoubleUnaryOperator f, double a, double b) {
+        //ваш код
+        return f.applyAsDouble(a);
+    }
+}
