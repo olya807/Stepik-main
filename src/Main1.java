c1cd43d6ea4f4390047d7810bc6429b352119dbf
Volha <morozolga92@gmail.com>
2024-04-07 13:16:32 +0300

    Task 5.2 - 2 code

--- src/Main1.java
+++ src/Main1.java
@@ -1,2 +1,17 @@
+import java.io.IOException;
+
 public class Main1 {
+
+    public static void main(String[] args) throws IOException {
+
+        int next = 0;
+        int prev = System.in.read();
+        while (prev != -1) {
+            next = System.in.read();
+            if (prev != 13 || next != 10) {
+                System.out.write(prev);
+            }
+            prev = next;
+        }
+        System.out.flush();}
 }
