7e58e01805440be1c2fe9314fa62890f0cbe7741
Volha <morozolga92@gmail.com>
2024-04-10 13:12:04 +0300

    Task 5.4 - code

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,26 @@
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class Animal {
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+        Animal[] animals = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
+            Integer numAnimals = ois.readInt();
+            animals = new Animal[numAnimals];
+            for (int i = 0; i < numAnimals; i++) {
+                animals[i] = (Animal) ois.readObject();
+            }
+        } catch (EOFException e) {
+            throw new IllegalArgumentException(e);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        } catch (ClassCastException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return animals;
+    }
 }
